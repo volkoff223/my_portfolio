61cc56f4dfb235267ba4b4f2048180a1ec2117ad
Nick Voelker <volkoff223@gmail.com>
2024-01-09 17:21:37 -0500

    Refactored and added components

---
+++ src/components/About.svelte
@@ -0,0 +1,29 @@
+<section
+	id="about"
+	class="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
+	aria-label="About me"
+>
+	<div
+		class="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0"
+	>
+		<h2 class="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">About</h2>
+	</div>
+	<div>
+		<p class="mb-4">
+			Back in 2012, Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat debitis quod a
+			voluptas minus officia assumenda sed sunt incidunt corrupti eaque, eveniet ex modi dolore
+			necessitatibus et. Repellendus veniam tempora, nemo deserunt expedita officia quisquam,
+			veritatis asperiores facere praesentium deleniti!
+		</p>
+		<p class="mb-4">
+			My main focus is Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi vero harum
+			laborum quas, temporibus modi dolores unde vitae hic saepe numquam quisquam minus distinctio
+			cupiditate placeat nemo pariatur doloribus accusantium!
+		</p>
+		<p>
+			When I’m not at the computer, Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quam
+			excepturi exercitationem, eaque aliquam nostrum dolorem totam, maxime dolores similique
+			quisquam eligendi qui illo molestias.
+		</p>
+	</div>
+</section>
