f9acd9d057bbc3199ee0d7fdba9b3fb0126c91cd
Nicholas Voelker <volkoff223@gmail.com>
2024-01-10 15:24:33 -0500

    Navbar fix

--- src/components/MainHeader.svelte
+++ src/components/MainHeader.svelte
@@ -16,6 +16,10 @@
       leading, managing and collaboration in the United States Navy.
     </p>
     <!-- ! Need to fix nav item focus -->
+    {#if 2 === 2}
+      <p>Working</p>
+    {/if}
+
     <nav class="nav hidden lg:block" aria-label="In-page jump links">
       <ul class="mt-16 w-max">
         <li>
@@ -29,7 +33,7 @@
           >
         </li>
         <li>
-          <a class="group flex items-center py-3 active" href="#projects"
+          <a class="group flex items-center py-3" href="#projects"
             ><span
               class="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"
             ></span><span
