c4a7e670590262b63a76affe0c687c5192c271dd
Nick Voelker <volkoff223@gmail.com>
2024-01-18 17:07:02 -0500

    updated projects

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -72,7 +72,7 @@
                     SvelteKit
                   </div>
                 </li>
-                <li>
+                <li class="mr-1.5 mt-2">
                   <div
                     class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300"
                   >
