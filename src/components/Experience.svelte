139ec98a35813edb7350b42b9861e9a5cd4ef770
Nick Voelker <volkoff223@gmail.com>
2024-01-25 20:08:17 -0500

    Updated Experience

--- src/components/Experience.svelte
+++ src/components/Experience.svelte
@@ -38,24 +38,24 @@
               <div>
                 <span>Navigation Division Manager</span>
               </div>
-              <div>
-                <div class="text-slate-500" aria-hidden="true">
-                  Senior Enlisted Leader
-                </div>
-              </div>
             </h3>
             <p class="mt-2 text-sm leading-normal">
-              Managed a department of 20 people comprised of administration,
-              medical, maintenance, and navigation personnel.
+              Salesforce database administrator responsible for user management,
+              permissions, providing end user support. Charged with data
+              maintenance, including building new functions, processes and
+              reports to improve efficiency for 300 users.
             </p>
             <p class="mt-2 text-sm leading-normal">
-              Lead training team member responsible for curriclum development
-              and delivery of instruction.
+              Developed an Excel spreadsheet to calculate and track daily
+              physical activity for 300 users. This spreadsheet was linked to an
+              automated dashboard that would display individual scores and group
+              rankings based on physical activity.
             </p>
             <p class="mt-2 text-sm leading-normal">
-              Database administrator responsible for transferring data from
-              paper format to spreadsheet and database systems for over 300
-              people.
+              Managed the professional growth of a department of 20 people
+              comprised of administration, medical, maintenance, and navigation
+              personnel. This included career counseling, periodic evaluations,
+              and oversight of day to day routines.
             </p>
           </div>
         </div>
@@ -78,24 +78,18 @@
               <div>
                 <span>High Risk Training Evaluator</span>
               </div>
-              <div>
-                <div class="text-slate-500" aria-hidden="true">
-                  Lead Training Team Member
-                </div>
-              </div>
             </h3>
             <p class="mt-2 text-sm leading-normal">
-              Spearheaded the construction of a one of a kind stand-alone mobile
-              radar and electronic navigation training system.
-            </p>
-            <p class="mt-2 text-sm leading-normal">
               Lead instructor responsible for the development of curriculum for
-              the hardware and software on the Navy's newest patrol boat.
+              electronic charting software called the Common Geospatial
+              Extensible Navigation Toolkit (COGENT) and the Garmin radar
+              systems.
             </p>
             <p class="mt-2 text-sm leading-normal">
-              Led three personnel as the inventory manager and maintained 100%
-              accountability of $1000,000 worth of navigation equipment used to
-              train the Navy.
+              Spearheaded the construction of a one of a kind stand-alone mobile
+              radar and electronic navigation training system. This allowed
+              instructors to increase class size fivefold by moving the class
+              from the boat to the classroom.
             </p>
           </div>
         </div>
@@ -118,32 +112,23 @@
               <div>
                 <span>Naval Special Warfare Research and Development</span>
               </div>
-              <div>
-                <div class="text-slate-500" aria-hidden="true">
-                  Survival Evasion Resistance and Escape (SERE) Specialist
-                </div>
-              </div>
             </h3>
             <p class="mt-2 text-sm leading-normal">
-              Collaborated with multiple government agencies responsible for the
-              development and testing of the newest technologie for geo-locating
-              services. This emergency beacon would prove to be so successful
-              that hundreds of personnel that deployed into harm's way carried
-              this device.
+              Collaborated with NSA, FBI, and CIA to design, development, and
+              support new products from concept to full production for ground to
+              Satellite Communication via radio frequency. One of the products
+              that went into production was a passive geo-location device.
             </p>
             <p class="mt-2 text-sm leading-normal">
-              Identified a bug in the database used for tracking the
-              geo-location tag. Troubleshoot with multiple agencies to determine
-              a solution for the error, and collaborated with the database
+              Identified how the SQL database used for the geo-location device
+              was storing date/time data improperly. Worked with the database
               engineer to implement the solution.
             </p>
             <p class="mt-2 text-sm leading-normal">
-              Field tested the device used to determine the position of the
-              geo-location tag from aircraft, vehicle and on foot. This testing
-              was done at sub-zero temperatures so each piece of hardware needed
-              to be carefully monitored and modified when it failed. This
-              testing gave confidence to the commanders to use the devices in
-              extreme temperatures.
+              Field tested the geo-location device from aircraft, vehicle and on
+              foot. This testing was done at sub-zero temperatures so each piece
+              of hardware needed to be carefully monitored and modified when it
+              failed.
             </p>
           </div>
         </div>
@@ -159,28 +144,71 @@
             class="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2"
             aria-label="2003 to 2013"
           >
-            2003 — 2013
+            2010 — 2013
           </header>
           <div class="z-10 sm:col-span-6">
             <h3 class="font-medium leading-snug text-slate-200">
               <div>
                 <span
-                  >Navigator | Patrol Boat Operator | Survival Instructor</span
+                  >Lead Instructor at SERE (Survival, Evasion, Resistance, and
+                  Escape) School</span
                 >
-              </div>
-              <div>
-                <div class="text-slate-500" aria-hidden="true">
-                  Junior Sailor
-                </div>
               </div>
             </h3>
             <p class="mt-2 text-sm leading-normal">
-              Served as a field craft instructor at the Navy’s survival school.
+              As the lead instructor at the Navy’s survival school. Utilized a
+              variety of blended learning techniques such as eLearning tools,
+              lectures, classroom discussions, team exercises, readings, case
+              studies, role-plays and demonstrations.
             </p>
+          </div>
+        </div>
+      </li>
+      <li class="mb-12">
+        <div
+          class="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50"
+        >
+          <div
+            class="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"
+          ></div>
+          <header
+            class="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2"
+            aria-label="2003 to 2013"
+          >
+            2006 — 2010
+          </header>
+          <div class="z-10 sm:col-span-6">
+            <h3 class="font-medium leading-snug text-slate-200">
+              <div>
+                <span>Riverine Patrol Boat Operator</span>
+              </div>
+            </h3>
             <p class="mt-2 text-sm leading-normal">
               Responsible for the safe navigation of Riverine patrol boats
               during two combat tours in Iraq.
             </p>
+          </div>
+        </div>
+      </li>
+      <li class="mb-12">
+        <div
+          class="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50"
+        >
+          <div
+            class="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"
+          ></div>
+          <header
+            class="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2"
+            aria-label="2003 to 2013"
+          >
+            2003 — 2006
+          </header>
+          <div class="z-10 sm:col-span-6">
+            <h3 class="font-medium leading-snug text-slate-200">
+              <div>
+                <span>Quartermaster</span>
+              </div>
+            </h3>
             <p class="mt-2 text-sm leading-normal">
               Responsible for the safe navigation of Navy war ships.
             </p>
