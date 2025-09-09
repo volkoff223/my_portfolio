3b4fbf59b5a76b93f3b957856079d47744766d7c
Nicholas Voelker <33676952+volkoff223@users.noreply.github.com>
2025-09-09 13:44:51 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import { mdsvex } from 'mdsvex';
+import { mdsvex } from "mdsvex";
 import adapter from "@sveltejs/adapter-netlify";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
