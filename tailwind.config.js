46c8ca95856d341add3814cf40a0966ca66ae488
Nicholas Voelker <33676952+volkoff223@users.noreply.github.com>
2025-09-09 12:44:49 -0400

    Update tailwind.config.js
    Added tailwind typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,svelte,js,ts}'],
+	content: ['./src/**/*.{html,svelte,js,ts,md,svx}'],
 	theme: {
 		extend: {}
 	},
-	plugins: [require('daisyui')],
+	plugins: [require('daisyui'),require('@tailwindcss/typography')],
 	daisyui: {
 		themes: ['night', 'cupcake', 'cyberpunk']
 	}
