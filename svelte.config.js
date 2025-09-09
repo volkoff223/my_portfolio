d71bcecd7032473f19fbc56a097f2157b5510498
Nicholas Voelker <33676952+volkoff223@users.noreply.github.com>
2025-09-09 12:27:34 -0400

    Update svelte.config.js
    Added mdsvex import.

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,17 @@
+import { mdsvex } from 'mdsvex';
 import adapter from "@sveltejs/adapter-netlify";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  prepprocess: vitePreprocess(),
+  extensions: ['.svelte', '.svx', '.md'],
+  prepprocess: [
+    vitePreprocess(),
+    mdsvex({
+      extensions: ['.svx', '.md']
+    })
+  ],
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
   },
 };
